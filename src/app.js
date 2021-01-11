2b3b800f26e765a08f8c8a376204479333f34512
clayohr <76801664+clayohr@users.noreply.github.com>
2021-01-11 13:37:29 -0500

    Cleaning code
    Removed vague class and id titles

--- src/app.js
+++ src/app.js
@@ -1,42 +1,4 @@
-let weather = {
-    paris: {
-      temp: 19.7,
-      humidity: 80
-    },
-    tokyo: {
-      temp: 17.3,
-      humidity: 50
-    },
-    lisbon: {
-      temp: 30.2,
-      humidity: 20
-    },
-    "san francisco": {
-      temp: 20.9,
-      humidity: 100
-    },
-    moscow: {
-      temp: -5,
-      humidity: 20
-    }
-  };
-  let now = new Date();
-  
-  //let city = prompt("Enter a city");
-  //  city = city.toLowerCase();
-  
-  //if (weather[city]!==undefined) {
-  // let tempy = weather[city].temp;
-  // let hum =weather[city].humidity;
-  // let celsiusTemp = Math.round(tempy);
-  //let fahrenheitTemp = Math.round((tempy*9)/5+32);
-  
-  //alert (`It is currently ${celsiusTemp}°C (${fahrenheitTemp} °F) in ${city} with a humidity of ${hum} %`);
-  //} else{
-  //alert("Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+" + city)
-  //};
-  
-  let days = [
+let days = [
     "Sunday",
     "Monday",
     "Tuesday",
@@ -51,12 +13,12 @@ let weather = {
   let hour = now.getHours();
   let minute = now.getMinutes();
   
-  let line = document.querySelector("#fuck");
+  let line = document.querySelector("#currentTime");
   line.innerHTML = `${day} ${hour}:${minute}`;
   
   function displayWeather(response) {
     document.querySelector("#location").innerHTML = response.data.name;
-    document.querySelector("#hone").innerHTML = Math.round(
+    document.querySelector("#currentWeather").innerHTML = Math.round(
       response.data.main.temp) + "°";
   }
   
